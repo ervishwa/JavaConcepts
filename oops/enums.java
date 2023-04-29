4a46dc5f66c4e943dad7d772d5afc5bd07d955f6
vishwjeet thakur <ervishwjeetthakur@gmail.com>
2023-04-29 16:55:37 +0530

    enum completed

--- oops/enums.java
+++ oops/enums.java
@@ -1,5 +1,15 @@
 package JavaConcepts.oops;
 
+enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+}
 public class enums {
+    public static void main(String[]args){
+        Level l1 = Level.MEDIUM;
+
+        System.out.println(l1);
+    }
 
 }
